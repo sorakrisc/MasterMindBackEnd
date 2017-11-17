4dd9cb3019af0782fe47c165181d001a724bbd1b
James Sorakris <james@Jamess-MacBook-Pro.local>
2017-11-17 12:12:40 +0700

    get request for validating answer

--- build.sbt
+++ build.sbt
@@ -15,7 +15,9 @@ libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
   "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
-  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+  "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+  "org.scalatra" % "scalatra-json_2.12" % "2.6.1",
+  "org.json4s"   %% "json4s-jackson" % "3.2.11"
 )
 
 enablePlugins(SbtTwirl)
--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -1,11 +1,69 @@
 package com.example.app
 
+import java.util.Date
+
 import org.scalatra._
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json.JacksonJsonSupport
+import org.json4s.jackson.JsonMethods._
+import org.json4s._
+import org.json4s.JsonDSL._
 
-class MyScalatraServlet extends ScalatraServlet {
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Breaks._
 
-  get("/") {
-    views.html.hello()
+// JSON-related libraries
+import org.json4s.{DefaultFormats, Formats}
+import scala.collection.mutable.{Map,
+  SynchronizedMap, HashMap}
+
+class MyScalatraServlet extends ScalatraServlet{
+
+  case class Flower(slug: String, name: String)
+  protected implicit lazy val jsonFormats: Formats = DefaultFormats
+  var map=HashMap[String, String]() //id,answer
+
+
+
+  def generateAnswer(): String ={
+    val choice = "roygbp"
+    var temp = ""
+    for (a <- 1 to 4){
+      val r = scala.util.Random.nextInt(6)
+      temp+= choice.charAt(r)
+    }
+    temp
+  }
+
+  def checkGuess(ans:String, guess:String): JObject ={
+    var totWhite = 0
+    var totRed = 0
+    var copguess = guess.to[ListBuffer]
+    for(i <- 0 until ans.length){
+      if(ans.charAt(i).equals(guess.charAt(i))) totRed+=1
+    }
+    for(i<- 0 until ans.length){
+      val ansc = ans.charAt(i)
+      breakable {
+        for (j <- 0 until copguess.length) {
+          if (ansc.equals(copguess(j))) {
+            copguess.remove(j)
+            totWhite += 1
+            break
+          }
+        }
+      }
+    }
+
+    ("white" -> math.abs(totWhite-totRed).toString) ~ ("red" -> totRed.toString)
+  }
+  get("/ans/:id"){
+    val id = params("id")
+    val guess = params("guess")
+    contentType = "application/json"
+    val js = (checkGuess(map.getOrElseUpdate(id, generateAnswer()),guess))
+    println(map.get(id))
+    compact(render(js))
   }
 
 }
